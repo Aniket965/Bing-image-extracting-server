1d44843cbe0d0699389e0a9dc445dc298eba9d98
aniket965 <aniket965.as@gmail.com>
2017-09-02 00:16:25 +0530

    updated!

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var axios = require('axios');
 var app = express();
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+next();
+}
+
+app.use(allowCrossDomain);
+
 app.get("/", (req, res) => {
   axios.get('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
-  .then(function (response) {
-   res.send("http://bing.com" + response.data.images[0].url);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(function (response) {
+      res.send({
+        url: "http://bing.com" + response.data.images[0].url
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 })
 var port = process.env.PORT || 3001
-app.listen(port,()=>console.log("listening to the port ",port))
-
+app.listen(port, () => console.log("listening to the port ", port))
