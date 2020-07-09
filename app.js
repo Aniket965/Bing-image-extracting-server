d74fa39aa45940c2f771dbc64fe5acdd599d4b25
aniket965 <aniket965.as@gmail.com>
2020-07-09 21:15:13 +0530

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-next();
+  next();
 }
 
 app.use(allowCrossDomain);
@@ -14,16 +14,21 @@ app.use(allowCrossDomain);
 app.get("/", (req, res) => {
   axios.get('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
     .then(function (response) {
-      request("http://bing.com" + response.data.images[0].url).pipe(res);
+      request("http://bing.com" + response.data.images[0].url)
+        .on("response", rese => {
+          // You can add/remove/modify headers here
+          setHeadersForCacheLength(rese, 0);
+        })
+        .pipe(res);
     })
     .catch(function (error) {
       console.log(error);
     });
 })
 
-function randomNumber(min, max) {  
-  return Math.random() * (max - min) + min; 
-}  
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 function setHeadersForCacheLength(res, cacheLengthSeconds) {
   const now = new Date()
@@ -49,23 +54,33 @@ function setHeadersForCacheLength(res, cacheLengthSeconds) {
 app.get("/unsplash1", (req, res) => {
 
   request("https://source.unsplash.com/random/800x400?space")
-  .on("response", rese => {
-    // You can add/remove/modify headers here
-    setHeadersForCacheLength(rese,0);
+    .on("response", rese => {
+      // You can add/remove/modify headers here
+      setHeadersForCacheLength(rese, 0);
+    })
+    .pipe(res);
 })
-  .pipe(res);
+
+app.get("/unsplash2", (req, res) => {
+
+  request("https://source.unsplash.com/random/800x200")
+    .on("response", rese => {
+      // You can add/remove/modify headers here
+      setHeadersForCacheLength(rese, 0);
+    })
+    .pipe(res);
 })
 app.get("/randomsvg2.svg", (req, res) => {
 
   res.set('Cache-Control', 'max-age=0, no-cache, no-store, must-revalidate'); // 1 year
 
-console.log(`https://placeholder.pics/svg/${randomNumber(300,400)}/`)
-  request(`https://placeholder.pics/svg/${randomNumber(300,400)}/`)
-  .on("response", rese => {
-    // You can add/remove/modify headers here
-    setHeadersForCacheLength(rese,0);
-})
-  .pipe(res);
+  console.log(`https://placeholder.pics/svg/${randomNumber(300, 400)}/`)
+  request(`https://placeholder.pics/svg/${randomNumber(300, 400)}/`)
+    .on("response", rese => {
+      // You can add/remove/modify headers here
+      setHeadersForCacheLength(rese, 0);
+    })
+    .pipe(res);
 })
 var port = process.env.PORT || 3001
 app.listen(port, () => console.log("listening to the port ", port))
