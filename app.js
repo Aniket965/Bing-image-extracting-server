2e05fc15ae32ba9f96992e4254ad9f7f113aeb89
aniket965 <aniket965.as@gmail.com>
2020-07-09 20:52:00 +0530

    feat:

--- app.js
+++ app.js
@@ -25,10 +25,32 @@ function randomNumber(min, max) {
   return Math.random() * (max - min) + min; 
 }  
 
+function setHeadersForCacheLength(res, cacheLengthSeconds) {
+  const now = new Date()
+  const nowGMTString = now.toGMTString()
+
+  // Send both Cache-Control max-age and Expires in case the client implements
+  // HTTP/1.0 but not HTTP/1.1.
+  let cacheControl, expires
+  if (cacheLengthSeconds === 0) {
+    // Prevent as much downstream caching as possible.
+    cacheControl = 'no-cache, no-store, must-revalidate'
+    expires = nowGMTString
+  } else {
+    cacheControl = `max-age=${cacheLengthSeconds}, s-maxage=${cacheLengthSeconds}`
+    expires = new Date(now.getTime() + cacheLengthSeconds * 1000).toGMTString()
+  }
+
+  res.setHeader('Date', nowGMTString)
+  res.setHeader('Cache-Control', cacheControl)
+  res.setHeader('Expires', expires)
+}
+
 app.get("/unsplash", (req, res) => {
   request("https://source.unsplash.com/random/800x400?space").pipe(res);
 })
 app.get("/randomsvg.svg", (req, res) => {
+  setHeadersForCacheLength(res,5);
 console.log(`https://placeholder.pics/svg/${randomNumber(300,400)}/`)
   request(`https://placeholder.pics/svg/${randomNumber(300,400)}/`).pipe(res);
 })
